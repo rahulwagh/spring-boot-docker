1b8b20cc96baba110eec4cd25dbcd7931a8decc3
rwagh <rahul.wagh@crosskey.fi>
2022-02-02 22:26:25 +0200

    updated one more rest service

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,7 +1,9 @@
 package com.jhooq.Jhooqk8s.ws;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class JhooqDockerDemoController {
@@ -10,4 +12,17 @@ public class JhooqDockerDemoController {
     public String hello() {
         return "Hello - Jhooq-k8s";
     }
+
+    @GetMapping("/callOtherApp")
+    public String otherApp() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://localhost:9090/makeOtherAppCall";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response.getBody();
+    }
+
+
 }
